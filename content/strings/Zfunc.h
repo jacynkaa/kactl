e33230eff73a0384ffcce2b8291b7cdbc32363d3
jacynkaa <jacynkaa@bitbucket.org>
2022-10-26 14:27:08 +0200

    add string alghoritms from algo-lib

--- content/strings/Zfunc.h
+++ content/strings/Zfunc.h
@@ -7,15 +7,17 @@
  */
 #pragma once
 
-vi Z(const string& S) {
-	vi z(sz(S));
-	int l = -1, r = -1;
-	rep(i,1,sz(S)) {
-		z[i] = i >= r ? 0 : min(r - i, z[i - l]);
-		while (i + z[i] < sz(S) && S[i + z[i]] == S[z[i]])
-			z[i]++;
-		if (i + z[i] > r)
-			l = i, r = i + z[i];
-	}
-	return z;
+vi Z(const string &S, bool z0n = false) {
+   vi z(sz(S));
+   int l = -1, r = -1;
+   rep(i, 1, sz(S)) {
+      z[i] = i >= r ? 0 : min(r - i, z[i - l]);
+      while (i + z[i] < sz(S) && S[i + z[i]] == S[z[i]])
+         z[i]++;
+      if (i + z[i] > r)
+         l = i, r = i + z[i];
+   }
+   if (z0n && sz(S))
+      z[0] = sz(S);
+   return z;
 }
