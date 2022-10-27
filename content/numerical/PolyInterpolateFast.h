afe1d6f8cf7c4086999f144150e18874a585bc02
jacynkaa <jacynkaa@bitbucket.org>
2022-10-27 16:53:20 +0200

    Polynomials, use blosoom for general matching

---
+++ content/numerical/PolyInterpolateFast.h
@@ -0,0 +1,17 @@
+#include "PolynomialPotepa.h"
+#pragma once
+
+/**
+ * Author: Krzysztof Potępa
+ * Description: Compute k-th term of an n-order linear
+ * recurrence C[i] = sum C[i-j-1]*D[j], given C[0..n-1] and D[0..n-1];
+ * Time: O(n log n log k)
+ */
+Zp linearRec(const Poly &C, const Poly &D, ll k) {
+   Poly f(sz(D) + 1, 1);
+   rep(i, 0, sz(D)) f[i] = -D[sz(D) - i - 1];
+   f = pow({0, 1}, k, f);
+   Zp ret = 0;
+   rep(i, 0, sz(f)) ret += f[i] * C[i];
+   return ret;
+}
